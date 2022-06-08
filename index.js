2ebe36b7454233c7223e0843ede98e2ccdf1b2ad
Arthur Pluym <tuur.pluym@gmail.com>
2022-06-08 12:17:46 +0200

    added options in the table

--- index.js
+++ index.js
@@ -19,30 +19,36 @@
 // ------------------------------------------------------------
 
     const breakPointsGallery = [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14
+        100,
+        200,
+        300,
+        400,
+        500,
+        600,
+        700,
+        800,
+        900,
+        1000,
+        1100,
+        1200,
+        1300,
+        1400
     ]
 // ------------------------------------------------------------
 
 // Variables
     var galleryIndex = 0
     var totalNumberOfCookies = 0
-    var autoAddAmount = 1
+    var autoAddAmount = 0
+    var clickAddAmount = 1
     var crackingAmount = 0
     var priceOptionOne = 20
-    var looop = window.setInterval(autoAdd, 1000)
+    var priceUpgrade = 30
+    var priceAutoClick = 500
+    var priceBoost = 10000
+    var loopAutoAdd = window.setInterval(autoAdd, 1000)
+    var loopICanBuy = window.setInterval(iCanBuy, 10)
+    var loopSave = window.setInterval(saveAll, 10)
 
 // ------------------------------------------------------------
 
@@ -55,12 +61,30 @@
     function autoAdd() {
         totalNumberOfCookies += autoAddAmount
         document.getElementById('numbers').innerHTML = `${totalNumberOfCookies} Cookies`
+        document.title = `${totalNumberOfCookies} Cookies`
     }
 // ------------------------------------------------------------
 
+    function iCanBuy() {
+        totalNumberOfCookies >= priceUpgrade ? document.getElementById('price-up').style.color = '#87d37c' : document.getElementById('price-up').style.color = '#d1953f'
+        totalNumberOfCookies >= priceAutoClick ? document.getElementById('price-auto').style.color = '#87d37c' : document.getElementById('price-auto').style.color = '#d1953f'
+        totalNumberOfCookies >= priceBoost ? document.getElementById('price-boost').style.color = '#87d37c' : document.getElementById('price-boost').style.color = '#d1953f' 
+    }
+
+    function saveAll() {
+        localStorage.setItem('totalNumberOfCookies', JSON.stringify(totalNumberOfCookies))
+    }
+
+    function load() {
+        console.log('I am loaded')
+        totalNumberOfCookies = parseInt(localStorage.getItem('totalNumberOfCookies'))
+        console.log(parseInt(localStorage.getItem('totalNumberOfCookies')))
+        document.getElementById('numbers').innerHTML = `${totalNumberOfCookies} Cookies`
+    }
+
 //
     document.getElementById("cookie").addEventListener("click", () => {
-        totalNumberOfCookies++
+        totalNumberOfCookies += clickAddAmount
         crackingAmount++
         if (crackingAmount == breakPointsGallery[galleryIndex]) {
             galleryIndex++
@@ -78,14 +102,42 @@
 
 //
     document.getElementById("optionone").addEventListener("click", () => {
-        if(totalNumberOfCookies >= priceOptionOne) {
-            autoAddAmount++
-            totalNumberOfCookies -= priceOptionOne
-            priceOptionOne = Math.floor(priceOptionOne * 1.3)
-            document.getElementById('price').innerHTML = `${priceOptionOne} Cookies`
-            document.getElementById('upgradeLevel').innerHTML = `${autoAddAmount} CpS`
+        load()
+    })
+
+    document.getElementById("up-btn").addEventListener("click", () => {
+        if(totalNumberOfCookies >= priceUpgrade) {
+            clickAddAmount++
+            totalNumberOfCookies -= priceUpgrade
+            priceUpgrade = Math.floor(priceUpgrade * 1.3)
+            document.getElementById('price-up').innerHTML = `${priceUpgrade}`
             document.getElementById('numbers').innerHTML = `${totalNumberOfCookies} Cookies`
         }
     })
+
+    document.getElementById('up-btn').onmouseup = function () {
+
+    }
+
+    document.getElementById("buy-btn").addEventListener("click", () => {
+        if(totalNumberOfCookies >= priceAutoClick) {
+            autoAddAmount++
+            totalNumberOfCookies -= priceAutoClick
+            priceAutoClick = Math.floor(priceAutoClick * 1.6)
+            document.getElementById('price-auto').innerHTML = `${priceAutoClick}`
+            document.getElementById('numbers').innerHTML = `${totalNumberOfCookies} Cookies`
+        }
+    })
+
+    document.getElementById("buy1btn").addEventListener("click", () => {
+        if(totalNumberOfCookies >= priceBoost) {
+            clickAddAmount *= 2
+            totalNumberOfCookies -= priceBoost
+            priceBoost = Math.floor(priceBoost * 2)
+            document.getElementById('price-boost').innerHTML = `${priceBoost}`
+            document.getElementById('numbers').innerHTML = `${totalNumberOfCookies} Cookies`
+        }
+    })
+
 })();
 // ------------------------------------------------------------
