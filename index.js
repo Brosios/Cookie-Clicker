e70c9da791525ccac6912e8b779927a600723968
Omar Maad <omarmaad90@gmail.com>
2022-06-08 09:04:21 +0200

    Modified and More Visually Clean

--- index.js
+++ index.js
@@ -1,23 +1,81 @@
 (() => {
 
+    const galleryCookies = [
+        './img/cook1.png',
+        './img/cook2.png',
+        './img/cook3.png',
+        './img/cook4.png',
+        './img/cook5.png',
+        './img/cook6.png',
+        './img/cook7.png',
+        './img/cook8.png',
+        './img/cook9.png',
+        './img/cook10.png',
+        './img/cook11.png',
+        './img/cook12.png',
+        './img/cook13.png',
+        './img/cook14.png'
+    ]
+// ------------------------------------------------------------
+    const breakPointsGallery = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14
+    ]
+// ------------------------------------------------------------
+
+// Variables
+    var galleryIndex = 0
     var totalNumberOfCookies = 0
     var autoAddAmount = 1
+    var crackingAmount = 0
     var priceOptionOne = 20
     var looop = window.setInterval(autoAdd, 1000)
 
+// ------------------------------------------------------------
+
+// 
     document.getElementById('numbers').innerHTML = `${totalNumberOfCookies} Cookies`
     document.getElementById('upgradeLevel').innerHTML = `${autoAddAmount} CpS`
+// ------------------------------------------------------------
 
+//
     function autoAdd() {
         totalNumberOfCookies += autoAddAmount
         document.getElementById('numbers').innerHTML = `${totalNumberOfCookies} Cookies`
     }
+// ------------------------------------------------------------
 
+//
     document.getElementById("cookie").addEventListener("click", () => {
         totalNumberOfCookies++
+        crackingAmount++
+        if (crackingAmount == breakPointsGallery[galleryIndex]) {
+            galleryIndex++
+            document.getElementById('pictureCookie').setAttribute('src', galleryCookies[galleryIndex])  
+        }
+        if (galleryIndex == galleryCookies.length) {
+            galleryIndex = 0
+            crackingAmount = 0
+            document.getElementById('pictureCookie').setAttribute('src', galleryCookies[galleryIndex])
+        }
+        
         document.getElementById('numbers').innerHTML = `${totalNumberOfCookies} Cookies`
     })
+// ------------------------------------------------------------
 
+//
     document.getElementById("optionone").addEventListener("click", () => {
         if(totalNumberOfCookies >= priceOptionOne) {
             autoAddAmount++
@@ -28,7 +86,5 @@
             document.getElementById('numbers').innerHTML = `${totalNumberOfCookies} Cookies`
         }
     })
-
-
-
 })();
+// ------------------------------------------------------------
